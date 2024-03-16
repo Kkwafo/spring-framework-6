ea0b452a53ed631aa01e0f083c16666910b4122b
Kkwafo <kkwafoawua@gmail.com>
2024-03-16 20:31:24 -0300

    Created JPA-Entities

---
+++ src/main/java/domain/Book.java
@@ -0,0 +1,41 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String isbc;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getIsbc() {
+		return isbc;
+	}
+	
+	public void setIsbc(String isbc) {
+		this.isbc = isbc;
+	}
+	
+}
