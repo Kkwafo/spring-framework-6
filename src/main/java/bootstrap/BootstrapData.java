259875620209b6769a58093825714f4f8e156c78
Kkwafo <kkwafoawua@gmail.com>
2024-03-17 14:31:04 -0300

    Adding publish to bootstrap

--- src/main/java/bootstrap/BootstrapData.java
+++ src/main/java/bootstrap/BootstrapData.java
@@ -5,18 +5,22 @@ import org.springframework.stereotype.Component;
 
 import domain.Author;
 import domain.Book;
+import domain.Publisher;
 import repositories.AuthorRepository;
 import repositories.BookRepository;
+import repositories.PublisherRepository;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,9 +32,20 @@ public class BootstrapData implements CommandLineRunner {
         Book ddd = new Book();
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
+        
+        Publisher pbs1 = new Publisher();
+        pbs1.setPublisherName("");
+        pbs1.setAddress("");
+        pbs1.setCity("");
+        pbs1.setState("");
+        pbs1.setZip("");
+        
+        publisherRepository.save(pbs1);
+
 
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
+        Publisher pbsSaved = publisherRepository.save(pbs1);
 
         Author rod = new Author();
         rod.setFirstName("Rod");
